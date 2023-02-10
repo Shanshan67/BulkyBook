f5f755c0e18d716895901006dec41579bb0a0ecd
sorya <sorya@DESKTOP-IDAP76P>
2023-02-10 19:24:17 +1100

    Add project files.

---
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();//return a view or redirect to some action method or rdirect a page
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();//routing will be done and it checked for authorization;if want to use authentication to pipeline,so
+//app.UseAuthentication();//验证，before authorization
+app.UseAuthorization();//授权
+
+app.MapControllerRoute(//map the different pattern that we have for MVC.Based on this routing, it will be able to redirect a request to the cossesponding controllers and action
+    name: "default",//have defined a default route
+    pattern: "{controller=Home}/{action=Index}/{id?}");//https://localhost:xxx/{controller}/{action}/{id}; after port number will be the route that we want to use
+
+app.Run();
+
+
